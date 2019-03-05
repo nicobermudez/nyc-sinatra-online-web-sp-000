ec5558048c83e6dabcd8857aa2500bd8f40241a2
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-05 17:59:38 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -2,8 +2,10 @@ class LandmarksController < ApplicationController
   # add controller methods
 
   get '/landmarks' do
-    @landmarks = Landmark.all
-    @figures = Figure.all
+    @landmarks = Landmark.all 
+    @figures = Figure.all 
     erb :'landmarks/index'
   end
+
+  get '/landmarks/:id'
 end
