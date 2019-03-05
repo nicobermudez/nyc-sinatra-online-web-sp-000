b5ce8c2d1b7bf745a000b3e60265b06b92499870
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-05 18:48:34 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -28,6 +28,8 @@ class FiguresController < ApplicationController
   patch'/figures/:id' do
     @figure = Figure.find(params[:id])
     @figure.update(params[:figure])
+    unless params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
 
     @figure.save
     redirect to "/figures/#{@figure.id}"
