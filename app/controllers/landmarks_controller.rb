4a980f6eed6c0b6593248c3879b48f9b2665f804
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-05 18:09:07 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -10,4 +10,9 @@ class LandmarksController < ApplicationController
   get '/landmarks/new' do
     erb :'/landmarks/new'
   end
+
+  post '/landmarks' do
+    Landmark.create(name: params['landmark']['name'], year_completed: params['landmark']['year_completed'])
+
+
 end
