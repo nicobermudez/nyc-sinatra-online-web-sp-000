1dc3a6f163ca8633a6a0f6ad25c25e1306874f57
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-05 17:26:37 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,7 +1,7 @@
 class CreateLandmarks < ActiveRecord::Migration
   def change
-    create_table :landmarks do |t| 
-      t.string :name 
+    create_table :landmarks do |t|
+      t.string :name
       t.string :year_completed
   end
 end
